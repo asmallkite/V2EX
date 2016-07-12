4c70f1777194b40bc5945eb25e8a3d51242b951d
lizheng <lixiaozheng.good@163.com>
2016-07-11 20:52:03 -0700

    add some package

--- app/src/main/java/com/example/a10648/v2ex/activity/MainActivity.java
+++ app/src/main/java/com/example/a10648/v2ex/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.a10648.v2ex;
+package com.example.a10648.v2ex.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.a10648.v2ex.R;
 
 public class MainActivity extends AppCompatActivity {
 
